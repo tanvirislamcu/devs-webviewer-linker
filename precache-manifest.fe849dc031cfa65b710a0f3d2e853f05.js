384a94847e0de76853aff2a430dd5638de85de2f
tanvirislamcu <tanvirislam@cunet.carleton.ca>
2021-04-21 12:57:15 -0400

    12:57:15

---
+++ precache-manifest.fe849dc031cfa65b710a0f3d2e853f05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927682e49afaf65fd36eaac1657b97ab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3ccef507981e4133e4cb",
+    "url": "./static/js/2.2ea33ab0.chunk.js"
+  },
+  {
+    "revision": "9c2ed165c4d62e68a4f3981f3879d2ce",
+    "url": "./static/js/2.2ea33ab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0922aa41c7420514b27",
+    "url": "./static/js/main.0f2de6e1.chunk.js"
+  },
+  {
+    "revision": "4787f54cd691be961ca0",
+    "url": "./static/js/runtime-main.d7ec947d.js"
+  },
+  {
+    "revision": "0bbc27c1ae0b27eedbcd002b1189977f",
+    "url": "./static/media/carleton.0bbc27c1.svg"
+  }
+]);
